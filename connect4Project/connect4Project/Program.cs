45de687829517edb08df58c8b63a47868704fa90
OkechukwuEbogu <okechukwuebogu@outlook.com>
2026-04-17 21:08:57 -0600

    Added skeleton structure
    Added skeleton structure

---
+++ connect4Project/connect4Project/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+namespace connect4Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            controller.StartGame();
+        }
+    }
+
+    class Controller
+    {
+        public void StartGame()
+        {
+            // Game flow will go here
+        }
+    }
+
+    class Model
+    {
+        public char[,] Board = new char[6, 7];
+
+        public void InitializeBoard()
+        {
+            // Initialize board
+        }
+    }
+
+    abstract class Player
+    {
+        public string Name { get; set; }
+        public char Symbol { get; set; }
+
+        public abstract int GetMove();
+    }
+
+    class HumanPlayer : Player
+    {
+        public override int GetMove()
+        {
+            return 0;
+        }
+    }
+
+    class View
+    {
+        public void DisplayBoard(char[,] board)
+        {
+            // Display logic here
+        }
+    }
+}
